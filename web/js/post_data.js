fbfb4132528d0313b934f7d9675bc5987620c0e8
luanhailiang <luan@luanhailiang.cn>
2015-02-14 08:01:54 -0800

    add some js function

--- web/js/post_data.js
+++ web/js/post_data.js
@@ -68,4 +68,34 @@ function pull_image() {
 
 function post_image(){
 
+	//判断是否有选择上传文件
+	var imgPath = $("#uploadFile").val();
+	if (imgPath == "") {
+	    alert("请选择上传图片！");
+	    return;
+	}
+	//判断上传文件的后缀名
+	var strExtension = imgPath.substr(imgPath.lastIndexOf('.') + 1);
+	if (strExtension != 'jpg' && strExtension != 'gif'
+	&& strExtension != 'png' && strExtension != 'bmp') {
+	    alert("请选择图片文件");
+	    return;
+	}
+	$.ajax({
+	    type: "POST",
+	    url: "handler/UploadImageHandler.ashx",
+	    data: { imgPath: $("#uploadFile").val() },
+	    cache: false,
+	    success: function(data) {
+	        alert("上传成功");
+	        // $("#imgDiv").empty();
+	        // $("#imgDiv").html(data);
+	        // $("#imgDiv").show();
+	    },
+	    error: function(XMLHttpRequest, textStatus, errorThrown) {
+	        alert("上传失败，请检查网络后重试");
+	    }
+	});
+
+
 }
