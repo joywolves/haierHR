152e56b5ed605ac1042ce9718cac3770d33c4797
luanhailiang <luan@luanhailiang.cn>
2015-02-24 18:54:16 +0800

    fix by chao

--- web/js/post_data.js
+++ web/js/post_data.js
@@ -12,9 +12,18 @@ var T_EC_EmpDetail = {};//人员明细表---------------------------------------
 	T_EC_EmpDetail["Gender"] = $("#Gender").val();                   //性别
 	T_EC_EmpDetail["Nation"] = $("#Nation").val();                   //民族
 	T_EC_EmpDetail["Religion"] = $("#Religion").val();               //宗教
+	T_EC_EmpDetail["Birthday"] = $("#Birthday").val();               //生日
+	
 	T_EC_EmpDetail["HukouLocation"] = $("#HukouLocation").val();     //户口
 	T_EC_EmpDetail["IdCardNo"] = $("#IdCardNo").val();               //身份证号
 	T_EC_EmpDetail["PassportNo"] = $("#PassportNo").val();           //护照号码
+	
+	T_EC_EmpDetail["Provience"] = $("#Provience").val();             //出生省份
+	T_EC_EmpDetail["MarriageDate"] = $("#MarriageDate").val();       //结婚日期
+	T_EC_EmpDetail["ChildNo"] = $("#ChildNo").val();      			 //子女数量	
+	T_EC_EmpDetail["Email"] = $("#Email").val();      				 //Email 
+	T_EC_EmpDetail["ZipCode"] = $("#ZipCode").val();      			 //邮政编码 
+	
 	T_EC_EmpDetail["FirstWorkDate"] = $("#FirstWorkDate").val();     //参加工作时间
 	T_EC_EmpDetail["Phone"] = $("#Phone").val();                     //移动电话
 	T_EC_EmpDetail["LCountryCode"] = $("#LCountryCode").val();       //居住国家
@@ -32,6 +41,14 @@ var T_EC_EmpDetail = {};//人员明细表---------------------------------------
 	T_EC_EmpDetail["SecondDegree"] = $("#SecondDegree").val();       //第二学位
 	T_EC_EmpDetail["HighDegree"] = $("#HighDegree").val();           //最高学历
 	
+	
+	T_EC_EmpDetail["FFName"] = $("#FFName").val();                     //家一名字
+	T_EC_EmpDetail["FFBirthday"] = $("#FFBirthday").val();           //
+	T_EC_EmpDetail["FFJob"] = $("#FFJob").val();           //
+	T_EC_EmpDetail["FFDetailAddress"] = $("#FFDetailAddress").val();         //
+	T_EC_EmpDetail["FFPhone"] = $("#FFPhone").val();       //
+	T_EC_EmpDetail["FFCityArea"] = $("#FFCityArea").val();       //
+	T_EC_EmpDetail["FFHaierBU"] = $("#FFHaierBU").val();           //
 
 	
 	
@@ -43,19 +60,25 @@ var T_EC_EntryEmpInfo = {};//导入信息补充表------------------------------
 	T_EC_EntryEmpInfo["HealthStatus"] = $("#HealthStatus").val();    //健康状况
 	T_EC_EntryEmpInfo["Award"] = $("#Award").val();                  //奖励
 	T_EC_EntryEmpInfo["Punishment"] = $("#Punishment").val();        //处分
+	T_EC_EntryEmpInfo["SpeSkill"] = $("#SpeSkill").val();        	//特长
 	
+
+var T_EC_EntryEmpInfoExtra = {};//个人信息拓展表---------------------------------------------	
+	T_EC_EntryEmpInfoExtra["ProfessionQualification"] = $("#ProfessionQualification").val();            //专业技术职务任职资格
+	T_EC_EntryEmpInfoExtra["ProfessionAwardDate"] = $("#ProfessionAwardDate").val();          		  //专业技术职务任职资格
 	
 	
 	var data = {}
 //	data["T_EC_Apply"] = T_EC_Apply; 主表只限于查询
 	data["T_EC_EmpDetail"] = T_EC_EmpDetail;
 	data["T_EC_EntryEmpInfo"] = T_EC_EntryEmpInfo;
+	data["T_EC_EntryEmpInfoExtra"] = T_EC_EntryEmpInfoExtra;
 	var msg = {}
 	msg["cmd"] = "insert_data";
 	msg["data"] = data;
 	$.post("../data.php",JSON.stringify(msg),
 	function(data,status){
-		alert("Data: " + data + "\nStatus: " + status);
+		alert("数据已保存成功！");
 	});
 }
 
@@ -72,9 +95,9 @@ function check_login () {
 
 	$.post("../data.php",JSON.stringify(msg),
 	function(data,status){
-	//	 alert("Data: " + data + "\nStatus: " + status);
-		if(data == "false"){
-			alert("IDCardNo not exist");
+		 //alert("Data: " + data + "\nStatus: " + status);
+		if(data == "false" ||data == "null" ){
+			alert("身份证号无效!");
 		}else{
 			window.location.href="./index.html";
 		}
@@ -94,9 +117,18 @@ var table;
 		$("#Gender").val(table["Gender"]);					//性别
 		$("#Nation").val(table["Nation"]);					//民族
 		$("#Religion").val(table["Religion"]);				//宗教
+		$("#Birthday").val(table["Birthday"]);	
 		$("#HukouLocation").val(table["HukouLocation"]);	//户口
 		$("#IdCardNo").val(table["IdCardNo"]);	            //身份证号
 		$("#PassportNo").val(table["PassportNo"]);	        //护照号码
+		$("#Provience").val(table["Provience"]);	        //出生省份
+		
+		$("#MarriageDate").val(table["MarriageDate"]);	
+		$("#ChildNo").val(table["ChildNo"]);	        
+		$("#Email").val(table["Email"]);	        	
+		$("#ZipCode").val(table["ZipCode"]);	        
+		
+
 		$("#FirstWorkDate").val(table["FirstWorkDate"]);	//参加工作时间
 		$("#Phone").val(table["Phone"]);	                //移动电话
 		$("#LCountryCode").val(table["LCountryCode"]);	    //居住国家
@@ -114,8 +146,13 @@ var table;
 		$("#SecondDegree").val(table["SecondDegree"]);	    //第二学位
 		$("#HighDegree").val(table["HighDegree"]);	        //最高学历
 		
-		
-		
+		$("#FFName").val(table["FFName"]);	                    //
+		$("#FFBirthday").val(table["FFBirthday"]);	                //
+		$("#FFJob").val(table["FFJob"]);	        //
+		$("#FFDetailAddress").val(table["FFDetailAddress"]);	    //
+		$("#FFPhone").val(table["FFPhone"]);	    //
+		$("#FFCityArea").val(table["FFCityArea"]);	        //
+		$("#FFHaierBU").val(table["FFHaierBU"]);	        //
 	}
 	//---------------------------------------------------------------------------
 	if(table = data["T_EC_EntryEmpInfo"]){
@@ -123,8 +160,16 @@ var table;
 		$("#HealthStatus").val(table["HealthStatus"]);			//健康状况
 		$("#Award").val(table["Award"]);			            //奖励
 		$("#Punishment").val(table["Punishment"]);			    //处分
+		$("#SpeSkill").val(table["SpeSkill"]);			  		  //特长
+		}
+	//------------------------------------------------------------------------------
+	if(table = data["T_EC_EntryEmpInfoExtra"]){	
+
+		$("#ProfessionQualification").val(table["ProfessionQualification"]);			  //专业技术职务任职资格
+		$("#ProfessionAwardDate").val(table["ProfessionAwardDate"]);			  		  //专业技术职务任职资格
+	}
+
 		
-		}	
 	//...set html val by sql data
 }
 
@@ -145,7 +190,7 @@ function pull_image() {
 
 	$.post("../data.php",JSON.stringify(msg),
 	function(data,status){
-		alert("Data: " + data + "\nStatus: " + status);
+		alert("数据已保存成功！");
 	});
 }
 
